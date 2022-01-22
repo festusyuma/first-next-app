3f9b942e06f1d0271274256ba8ed176d6ec68f3a
festusyuma <festusyuma@gmail.com>
2022-01-22 23:17:19 +0100

    dynamic route

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+const Article = ({ article }) => {
+    return (
+        <>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br/>
+            <Link href={'/'}>Go Back</Link>
+        </>
+    )
+}
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+    const paths = articles.map(article => ({ params: { id: article.id.toString() } }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default Article
