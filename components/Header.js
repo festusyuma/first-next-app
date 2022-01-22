61d7476ff6925e49148c11455b5f493128d0ccea
festusyuma <festusyuma@gmail.com>
2022-01-22 22:43:57 +0100

    updates

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
   return (
       <div className={headerStyles.title}>
           <h1><span>WebDev</span> News</h1>
+          <p className={headerStyles.description}>Keep up to date with the latest web dev news</p>
       </div>
   )
 }
